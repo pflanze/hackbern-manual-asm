8bdd9cdbb92c596f82f4745fb65231796657489f
Christian Jaeger <ch@christianjaeger.ch>
2015-01-21 23:56:24 +0100

    simplify

--- foo.s
+++ foo.s
@@ -1,14 +1,7 @@
 	.globl	main
 main:
-	.cfi_startproc
 	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
 	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
 	movl	$7, %eax
 	popl	%ebp
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
 	ret
-	.cfi_endproc
