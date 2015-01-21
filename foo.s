269bee0335cdd7cf2d57cc3c199ac506909f172d
Christian Jaeger <ch@christianjaeger.ch>
2015-01-22 00:46:52 +0100

    (blah)
    foo.s:7: Error: no such instruction: `eax ->eax'

--- foo.s
+++ foo.s
@@ -1,5 +1,26 @@
+#define false 0
+#define	true 1
+
 	.intel_syntax noprefix
 	.globl	main
-main:
-	mov eax,0
+
+oddP: ;; eax -> eax
+	cmp eax,0
+	bne oddP_1
+	mov eax, false
 	ret
+oddP_1:	sub eax,1
+	jmp evenP
+	
+evenP:	
+	cmp eax,0
+	beq evenP_end
+	sub eax,1
+	jmp oddP
+evenP_end:
+	mov eax, true
+	ret
+	
+main:
+	mov eax,7
+	jmp evenP
