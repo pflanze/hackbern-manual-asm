22d8cdbe9d43b755f8c8eca65252aedbce10fb76
Christian Jaeger <ch@christianjaeger.ch>
2015-01-28 17:48:12 +0100

    eliminate CFI directives
    gcc -Wall -fno-asynchronous-unwind-tables -masm=intel -S -o hello_C.s hello_C.c

--- hello_C.s
+++ hello_C.s
@@ -7,24 +7,15 @@
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
-	.cfi_startproc
 	push	ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
 	mov	ebp, esp
-	.cfi_def_cfa_register 5
 	and	esp, -16
 	sub	esp, 16
 	mov	DWORD PTR [esp], OFFSET FLAT:.LC0
 	call	puts
 	mov	eax, 0
 	leave
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
 	ret
-	.cfi_endproc
-.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Debian 4.7.2-5) 4.7.2"
 	.section	.note.GNU-stack,"",@progbits
